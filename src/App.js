49bdcf2345d8c1d51b84053927025a2af0821dc8
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2022-11-29 23:07:25 +0100

    add useMemo and make 2 counters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
-import "./style.css";
+import React, { useState, useMemo } from 'react';
+import './style.css';
+import Counter from './Counter';
 
-export default function App() {
+const App = () => {
+  const [firstCounter, setFirstCounter] = useState(0);
+  const [secondCounter, setSecondCounter] = useState(0);
+
+  const increaseFirstCounter = () => setFirstCounter(firstCounter + 1);
+  const increaseSecondCounter = () => setSecondCounter(secondCounter + 1);
+
+  const firstCounterComponent = useMemo(
+    () => <Counter counter={firstCounter} index={1} />,
+    [firstCounter]
+  );
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>Licznik nr 1,wynosi: {firstCounter}</p>
+      <p>Licznik nr 2,wynosi: {secondCounter}</p>
+      {/* {firstCounterComponent} */}
+      <Counter counter={firstCounter} index={1} />
+      <Counter counter={secondCounter} index={2} />
+
+      <button onClick={increaseFirstCounter}>Zwiększ licznik nr.1</button>
+      <button onClick={increaseSecondCounter}>Zwiększ licznik nr.2</button>
     </div>
   );
-}
+};
+
+export default App;
